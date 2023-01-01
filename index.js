d744dc7a506c802144b7f22ecccfb0a87e6db5e8
satyaezzy <satyaezzy@gmail.com>
2023-01-01 14:58:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var name = prompt('masukan nama title/judul halaman website ini. NAME TITLE ? ','');
+
+if(name == 'ezzy'){
+    alert('selamat datang kembali');
+}else{
+    window.location="https://google.com";
+}
