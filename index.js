ad9953de74107d026a2624aaf4986badf322ce3d
satyaezzy <121744995+satyaezzy@users.noreply.github.com>
2023-01-01 15:19:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var name = prompt('masukan nama title/judul halaman website ini. NAME TITLE ? ','');
 
-if(name == 'ezzy'){
+if(name == 'kayla'){
     alert('selamat datang kembali');
 }else{
     window.location="https://google.com";
