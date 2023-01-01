5613a4938bf3d7d8dbf532e7519b06a9ce669fc0
satyaezzy <121744995+satyaezzy@users.noreply.github.com>
2023-01-01 21:47:27 +0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-alert('selamat datang kembali');
+alert('website ini masih dalam tahap pengembangan developer');
+console.log('masih blm responsive');
+console.log('data diri saya belum ada');
+console.log('transisi blm ad, begitu juga animasi');
